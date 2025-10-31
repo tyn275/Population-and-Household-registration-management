923c30b651c46db5643fcfdef5132467ccd776ae
Bminh2005 <binhminh4102005@gmail.com>
2025-10-31 22:20:22 +0700

    2025-10-31 database and tool

--- Database/DataGenerator/InsertAccountsSQL.py
+++ Database/DataGenerator/InsertAccountsSQL.py
@@ -2,6 +2,6 @@ import random
 with open("./insertAccountsSQL.sql", "w", encoding="utf-8") as f, open("./CCCD/cccd.txt", "r", encoding="utf-8") as g:
     for i in range(50):
         cccd = g.readline().strip()
-        sql = f"INSERT INTO accounts VALUES ({cccd}, '123456', 'cu dan');\n"
+        sql = f"INSERT INTO accounts VALUES ('{cccd}', '123456', 'cu dan');\n"
         f.write(sql)
 
--- Database/DataGenerator/insertNhanKhauHoKhau.py
+++ Database/DataGenerator/insertNhanKhauHoKhau.py
@@ -2,7 +2,7 @@ import random
 with open("./insertNhanKhauHoKhauSQL.sql", "w", encoding="utf-8") as f, open("./CCCD/cccd.txt", "r", encoding="utf-8") as g, open("./HoTen/hoTen.txt", "r", encoding="utf-8") as t, \
     open("./gioiTinh/gioiTinh.txt", "r", encoding="utf-8") as gt, open("./ngaySinh/ngaySinh.txt", "r", encoding="utf-8") as ns,\
         open("./queQuan/queQuan.txt", "r", encoding="utf-8") as qq:
-    for i in range(10):
+    for i in range(10): #chỉnh dòng này thành in range(11, 20) (21, 30) tùy vào số lượng hộ hiện tại 
         sql = f"INSERT INTO ho_khau VALUES ({i+1}, '{random.randint(1, 20)} ngach {random.randint(1, 50)}', null);\n"
         f.write(sql)
         for j in range(5):
@@ -24,5 +24,5 @@ with open("./insertNhanKhauHoKhauSQL.sql", "w", encoding="utf-8") as f, open("./
                 quanhe = 'vo'
             if (j > 1):
                 quanhe = 'con ruot'
-            sql = f"INSERT INTO nhan_khau VALUES ('{cccd}' , '{id_ho_khau}' , '{hoTen}' , null ,  '{gioiTinh}' , '{ngaySinh}' , '{que}' , '{que}' , 'kinh' , null , '{quanhe}' , null , '{que}' , '2025-10-31', '2025-10-31' , null , {cccd});\n"
+            sql = f"INSERT INTO nhan_khau VALUES ('{cccd}' , '{id_ho_khau}' , '{hoTen}' , null ,  '{gioiTinh}' , '{ngaySinh}' , '{que}' , '{que}' , 'kinh' , null , '{quanhe}' , null , '{que}' , '2025-10-31', '2025-10-31' , null , '{cccd}');\n"
             f.write(sql)
